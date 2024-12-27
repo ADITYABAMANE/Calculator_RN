f8312cb08c728a5afed7c92f889c38aa0d6a5408
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2024-12-27 17:23:48 +0530

    keyboard.js

--- Calculator_react_native/calculator/components/keyboard/keyboard.js
+++ Calculator_react_native/calculator/components/keyboard/keyboard.js
@@ -6,12 +6,14 @@ import MathOperBtn from "./components/mathOperBtn";
 
 const Keyboard = (props) => {
   const { oper, setOper } = props;
+
   const numClickHandler = (num) => {
     if (oper.length > 14)
       return Alert.alert("Warning", "characters must be less than 15");
     if (oper.length == 0 && num == 0) return;
     setOper(oper + num);
   };
+
   const funBtnClickHandler = (name) => {
     if (name == "delte") {
       return setOper(oper.slice(0, oper.length - 1));
@@ -20,11 +22,11 @@ const Keyboard = (props) => {
       setOper("");
     }
   };
+
   const mathOperBtnHandler = (name) => {
     if (oper.length > 14)
       return Alert.alert("Warning", "characters must be less than 15");
-    if(isNaN(oper[oper.length-1]))
-      return
+    if (isNaN(oper[oper.length - 1])) return;
     if (name == "+") {
       setOper(oper + "+");
     }
@@ -38,8 +40,9 @@ const Keyboard = (props) => {
       setOper(oper + "/");
     }
   };
+
   const runOperHadler = () => {
-    let isRun = true
+    let isRun = true;
     oper
       .toString()
       .split("")
@@ -53,52 +56,51 @@ const Keyboard = (props) => {
           element === "%" ||
           element === "."
         ) {
-        }else{
-          isRun=false
+        } else {
+          isRun = false;
         }
       });
-      if (isRun && !isNaN(oper.toString()[oper.toString().length - 1])) {
-        let result = eval(oper);
-        (result.toString().length>15)? 
-          setOper(result.toString().slice(0, 15)):
-          setOper(result.toString())
-          return result;
-      }
+    if (isRun && !isNaN(oper.toString()[oper.toString().length - 1])) {
+      let result = eval(oper);
+      result.toString().length > 15
+        ? setOper(result.toString().slice(0, 15))
+        : setOper(result.toString());
+      return result;
+    }
   };
-  const extraNumOper = (name)=>{
-    if(name=='.'){
-      if(oper.length>0 && !isNaN(oper[oper.length-1]))
-        setOper(oper+'.')
-      if(oper.length==0)
-        setOper(oper+'0.')
-      return
-    }  
-    if(name=='+/-'){
-      if(oper.slice(0,5)!=='(-1)*'){
-        setOper('(-1)*'+oper)
-      }else{
-        setOper(oper.slice(5))
-      }
-    }  
 
-  }
-  const extraMathOper = async (name)=>{
-    let result;
-    if(name=='square'){
-      result = await runOperHadler()
-      setOper(Math.sqrt(Number(result)))
+  const extraNumOper = (name) => {
+    if (name == ".") {
+      if (oper.length > 0 && !isNaN(oper[oper.length - 1])) setOper(oper + ".");
+      if (oper.length == 0) setOper(oper + "0.");
       return;
     }
-    if(name=='residual'){
-      result=oper
-      if(!isNaN(Number(oper))){
-        result = await runOperHadler()
-        setOper(oper+'%')
+    if (name == "+/-") {
+      if (oper.slice(0, 5) !== "(-1)*") {
+        setOper("(-1)*" + oper);
+      } else {
+        setOper(oper.slice(5));
+      }
+    }
+  };
+
+  const extraMathOper = async (name) => {
+    let result;
+    if (name == "square") {
+      result = await runOperHadler();
+      setOper(Math.sqrt(Number(result)));
+      return;
+    }
+    if (name == "residual") {
+      result = oper;
+      if (!isNaN(Number(oper))) {
+        result = await runOperHadler();
+        setOper(oper + "%");
         return;
       }
-        
     }
-  }
+  };
+
   return (
     <View style={[styles.keyboard, styles.column]}>
       <View style={{ position: "relative", left: "85%" }}>
@@ -112,12 +114,18 @@ const Keyboard = (props) => {
           style={styles.btn}
           onPress={() => funBtnClickHandler("clear")}
         >
-          <Text style={[styles.btnText, {color: '#f00'}]}>C</Text>
+          <Text style={[styles.btnText, { color: "#f00" }]}>C</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.btn} onPress={()=>extraMathOper('square')}>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => extraMathOper("square")}
+        >
           <MaterialCommunityIcons name="square-root" size={40} color="#000" />
         </TouchableOpacity>
-        <TouchableOpacity style={styles.btn} onPress={()=>extraMathOper('residual')}>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => extraMathOper("residual")}
+        >
           <Text style={styles.btnText}>%</Text>
         </TouchableOpacity>
         <MathOperBtn content={"/"} setNums={mathOperBtnHandler} />
@@ -141,16 +149,20 @@ const Keyboard = (props) => {
         <MathOperBtn content={"+"} setNums={mathOperBtnHandler} />
       </View>
       <View style={[styles.keyGroup, styles.row, styles.between]}>
-        <TouchableOpacity style={styles.btn} onPress={()=>extraNumOper('+/-')}>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => extraNumOper("+/-")}
+        >
           <Text style={styles.btnText}>+/-</Text>
         </TouchableOpacity>
-        <Nums content={0} setNums={numClickHandler}/>
-        <TouchableOpacity style={styles.btn} onPress={()=>extraNumOper('.')}>
+        <Nums content={0} setNums={numClickHandler} />
+        <TouchableOpacity style={styles.btn} onPress={() => extraNumOper(".")}>
           <Text style={styles.btnText}>,</Text>
         </TouchableOpacity>
         <TouchableOpacity
-          style={[styles.btn, { backgroundColor: "#0f0" }]}
-          onPress={runOperHadler}>
+          style={[styles.btn, { backgroundColor: "#0f0" }]} 
+          onPress={runOperHadler}
+        >
           <Text style={styles.btnText}>=</Text>
         </TouchableOpacity>
       </View>
